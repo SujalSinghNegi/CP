a5a70ab91c4b09fa1edb698f8d70d402ee877b06
Sujal Singh Negi <negisujal2007@gmail.com>
2025-03-15 20:26:21 +0530

    added the files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
 using namespace std;
+bool chk_odd( int a){
+    if(a%2==0){
+        return false;
+    }
+    return true;
+}
+int main(){
+    int t;
+    cin>>t;
+    int i=0;
+    while(i<t){
+    int n;
+    cin>>n;
 
-int main() {
-   
-   int n;
-   cin>>n;
-  
-    cout<< (((n+3)%4 && (n+2)%4)? "EVEN" : "ODD") <<endl;
-  
-    
-    return 0;
+    int chk = (n-1)/2;
+    if(chk_odd(n)){
+        if(chk_odd(chk)){
+            cout<<"even"<<endl;
+        }
+        else{
+            cout<<"odd"<<endl;
+        }
+    }
+    else{
+        if(chk_odd(n/2)){
+            cout<<"odd"<<endl;
+        }
+        else{
+            cout<<"even"<<endl;
+        }
+    }
+    i++;
+    }
+   return 0;
 }
